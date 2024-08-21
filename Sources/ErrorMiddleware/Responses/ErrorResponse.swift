d88e38678e21a8b8cf3e8a88497509f5dfa37d5a
Mykola Buhaiov <gulivero1773@icloud.com>
2024-08-21 12:11:25 +0300

    fix: delete not use

--- Sources/ErrorMiddleware/Responses/ErrorResponse.swift
+++ Sources/ErrorMiddleware/Responses/ErrorResponse.swift
@@ -63,14 +63,3 @@ public struct ErrorResponse: Content {
         self.errorUri = errorUri
     }
 }
-
-extension ErrorResponse {
-   public enum CodingKeys: String, CodingKey {
-        case isError = "is_error"
-        case reason
-        case error
-        case status
-        case code
-        case errorUri = "error_uri"
-    }
-}
