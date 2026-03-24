ecf07145bea10f7ba5b5388a4b14faae7169ca2c
Mykola Buhaiov <gulivero1773@icloud.com>
2026-03-24 19:59:33 +0200

    fix: add workflow for github

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.65.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
